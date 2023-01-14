2a76f1f358c745bae6b603f6002a164fe5e25b48
balves <balves@lopes.com.br>
2023-01-13 18:45:57 -0800

    Folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './pages/login-screen/login-screen.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
+import { ButtonComponent } from './shared/button/button.component';
+import { InputComponent } from './shared/input/input.component';
+import { CardContactComponent } from './shared/card-contact/card-contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegisterComponent,
+    ContactListComponent,
+    ButtonComponent,
+    InputComponent,
+    CardContactComponent
   ],
   imports: [
     BrowserModule,
