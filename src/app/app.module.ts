d9b3d6c6bec446bdf8060df45b20808034165ee7
balves <balves@lopes.com.br>
2023-01-14 19:09:42 -0800

    backend setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ButtonComponent } from './shared/button/button.component';
 import { InputComponent } from './shared/input/input.component';
 import { CardContactComponent } from './shared/card-contact/card-contact.component';
 import { IntroIllustrationComponent } from './shared/intro-illustration/intro-illustration.component';
+import { fakeBackendProvider } from './support-utility/local-backend';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -24,9 +27,13 @@ import { IntroIllustrationComponent } from './shared/intro-illustration/intro-il
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
