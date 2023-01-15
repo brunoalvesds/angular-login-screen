0f9b88b88bd1460247601358d737f66c47c0ce4a
balves <balves@lopes.com.br>
2023-01-14 21:15:18 -0800

    Auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  login(credentials: any) {
+    return this.http.post('/api/authenticate', 
+      JSON.stringify(credentials))
+      .pipe( 
+        map( (response: any) => {
+          let result = response;
+          if(result && result.token) {
+            localStorage.setItem('token', result.token);
+            return true;
+          }
+          return false;
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn() {
+    let jwtHelper = new JwtHelperService;
+    let token = localStorage.getItem('token');
+    return !jwtHelper.isTokenExpired(token);
+    // 
+    // if(!token) 
+    //   return false;
+    
+    // let expirationDate = jwtHelper.getTokenExpirationDate(token);
+    // let isExpired = jwtHelper.isTokenExpired(token);
+
+    // return !isExpired;
+  }
+
+  getCurrentUser() {
+    let token = localStorage.getItem('token');
+    if(!token) return null;
+    return new JwtHelperService().decodeToken(token);
+  }
+
 }
--- src/app/pages/login-screen/login-screen.component.ts
+++ src/app/pages/login-screen/login-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-login-screen',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-screen.component.scss']
 })
 export class LoginScreenComponent implements OnInit {
+  invalidLogin: boolean = false;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authSevice: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  signIn(credentials: any) {
+    console.log("cred: ", credentials);
+    this.authSevice.login(credentials).subscribe(result => {
+        if(result) {
+          let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl');
+
+          this.router.navigate([returnUrl || '/']);
+        }
+        this.invalidLogin = true;
+      });
+
+      
   }
 
 }
