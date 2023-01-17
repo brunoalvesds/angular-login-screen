595dcfd2613e5c026f8b772df95be341de7be05e
balves <balves@lopes.com.br>
2023-01-17 03:29:43 -0800

    Angular upgrade

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,28 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) { }
 
   login(credentials: any) {
     return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials))
-      .pipe( 
-        map( (response: any) => {
-          let result = response;
-          if(result && result.token) {
-            localStorage.setItem('token', result.token);
-            return response;
-          }
-          return false;
-        })
-      );
+    JSON.stringify(credentials))
+    .pipe( 
+      map( (response: any) => {
+        let result = response;
+        if(result && result.token) {
+          localStorage.setItem('token', result.token);
+          return response;
+        }
+        return false;
+      })
+    );
   }
 
   logout() {
